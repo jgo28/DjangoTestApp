7a55ac17278eaec02b65cde5fbdb42df8b71a9eb
jgo98 <gojoshua98@gmail.com>
2019-09-03 12:41:43 -0700

    added polls view page

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
+'''
+    The include() function allows referencing other URLconfs. Whenever Django 
+    encounters include(), it chops off whatever part of the URL matched up to 
+    that point and sends the remaining string to the included URLconf for 
+    further processing.
+'''
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
