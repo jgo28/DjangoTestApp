19d76450071bd06186fd5eeeca9475e7bfff8639
jgo98 <gojoshua98@gmail.com>
2019-09-03 13:36:41 -0700

    basic poll app

--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,8 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
+    def __str__(self):
+        return self.question_text
 
 '''
     A Choice has two fields: the text of the choice and a vote tally.
@@ -17,3 +18,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
