0cd0fb80d9a03e49f950a41da07d223fc0c6bebc
jgo98 <gojoshua98@gmail.com>
2019-09-03 13:12:50 -0700

    polls view

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+'''
+    A Question has a question and a publication date.
+'''
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+'''
+    A Choice has two fields: the text of the choice and a vote tally.
+    Each Choice is associated with a Question.
+'''
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
